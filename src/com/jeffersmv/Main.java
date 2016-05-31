6a81555dd75dbe8e69c0bd4a7cb35f0c68916b16
Viktor <jeffersmv@gmail.com>
2016-05-31 07:05:36 +0300

    ver 2

--- src/com/jeffersmv/Main.java
+++ src/com/jeffersmv/Main.java
@@ -1,6 +1,8 @@
 package com.jeffersmv;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,7 +18,47 @@ public class Main {
 	// write your code here
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance(); //возвращает объект DBF
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder(); // возвращает объект DB
-        Document document = documentBuilder.parse(new File("src/com.jeffersmv/CompanyStaff.xml")); //DOM выстроил все теги
+        File file = new File("src/com/jeffersmv/CompanyStaff.xml");
+        Document document = documentBuilder.parse(file); //DOM выстроил все теги
+
+        Element element = document.getDocumentElement(); //получение корневого тега
+        String stringElement = element.getNodeName(); //Возвращает имя элемента (тега)
+        System.out.println("stringElement = " + stringElement);
+
+
+        NodeList nodeList = document.getElementsByTagName("worker");
+        String fullName = "";
+        String date_of_Birth = "";
+        String date_Recruited = "";
+        String worker = "";
+
+        System.out.println("workers");
+        System.out.println("managers");
+        System.out.println("otherEmployees");
+        System.out.println();
+        System.out.println("Worker id = |" + "    FullName   |" + " Date Of Birth |" + " Date Recruited |");
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Element element1 = (Element) nodeList.item(i);
+            fullName = element1.getElementsByTagName("fullName").item(0).getChildNodes().item(0).getNodeValue();
+            date_of_Birth = element1.getElementsByTagName("date_of_Birth").item(0).getChildNodes().item(0).getNodeValue();
+            date_Recruited = element1.getElementsByTagName("date_Recruited").item(0).getChildNodes().item(0).getNodeValue();
+            worker = element1.getAttributes().getNamedItem("id").getNodeValue();
+
+
+            System.out.println(worker + "             "  + fullName + "     " + date_of_Birth + "      "+ date_Recruited + "       "+ element1.getNodeName());
+        }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
